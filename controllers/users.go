a3ab048a7cf51ee8c4b88f97aa8fdf6acb5e2b15
Ali Rido <mohalirido@gmail.com>
2021-02-15 22:02:45 +0700

    Updated update user method

--- controllers/users.go
+++ controllers/users.go
@@ -1,26 +1,27 @@
 package controllers
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
+
 	// "log"
 
+	"github.com/Alirido/SV-user-management/models"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"golang.org/x/crypto/bcrypt"
-	"github.com/Alirido/SV-user-management/models"
 )
 
 type CreateUserInput struct {
 	Username string `json:"username" binding:"required,min=3"`
 	Password string `json:"password" binding:"required,min=7"`
-	Name string `json:"name" binding:"required,min=3"`
+	Name     string `json:"name" binding:"required,min=3"`
 }
 
 type UpdateUserInput struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
-	Name string `json:"name""`  
+	Name     string `json:"name""`
 }
 
 // BeforeUpdate : hook before a user is updated
@@ -29,11 +30,11 @@ func (u *UpdateUserInput) BeforeUpdate(scope *gorm.Scope) (err error) {
 	fmt.Println(u.Password)
 
 	if u.Password != "" && len(u.Password) >= 7 {
-			hash, err := HashAndSaltPwd(u.Password)
-			if err != nil {
-					return nil
-			}
-			scope.SetColumn("Password", hash)
+		hash, err := HashAndSaltPwd(u.Password)
+		if err != nil {
+			return nil
+		}
+		scope.SetColumn("Password", hash)
 	}
 
 	fmt.Println(u.Password)
@@ -57,14 +58,14 @@ func GetUsers(c *gin.Context) {
 // Endpoint: GET /users/:id
 // Get an user
 func GetUser(c *gin.Context) {
-  var user models.User
+	var user models.User
 
-  if err := models.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-    return
-  }
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
 
-  c.JSON(http.StatusOK, gin.H{"data": user})
+	c.JSON(http.StatusOK, gin.H{"data": user})
 }
 
 // Endpoint: POST /users
@@ -92,37 +93,46 @@ func CreateUser(c *gin.Context) {
 // Endpoint: PATCH /users/:id
 // Update an user
 func UpdateUser(c *gin.Context) {
-  // Get model if exist
-  var user models.User
-  if err := models.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-    return
-  }
+	// Get model if exist
+	var user models.User
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
 
-  // Validate input
-  var input UpdateUserInput
-  if err := c.ShouldBindJSON(&input); err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-    return
+	// Validate input
+	var input UpdateUserInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if input.Password != "" {
+		hashedPassword, err := HashAndSaltPwd(input.Password)
+		if err != nil {
+			c.JSON(http.StatusServiceUnavailable, gin.H{"error": err.Error()})
+		}
+
+		input.Password = hashedPassword
 	}
 
 	// Update and Save user
 	models.DB.Model(&user).Updates(input)
 
-  c.JSON(http.StatusOK, gin.H{"data": user})
+	c.JSON(http.StatusOK, gin.H{"data": user})
 }
 
 // DELETE /users/:id
 // Delete a user
 func DeleteUser(c *gin.Context) {
-  // Get model if exist
-  var user models.User
-  if err := models.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
-    c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
-    return
-  }
+	// Get model if exist
+	var user models.User
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&user).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Record not found!"})
+		return
+	}
 
-  models.DB.Delete(&user)
+	models.DB.Delete(&user)
 
-  c.JSON(http.StatusOK, gin.H{"data": "Successfully deleted user with ID:" + c.Param("id")})
+	c.JSON(http.StatusOK, gin.H{"data": "Successfully deleted user with ID:" + c.Param("id")})
 }
